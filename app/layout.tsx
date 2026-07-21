2d1816d20db69d000caccd71b2db70fb58cdf6cb
smkwon-gaia3d <smkwon@gaia3d.com>
2026-07-21 14:38:27 +0900

    Improve small-text readability

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import "./product.css";
 import "./features.css";
 import "./best-enhancements.css";
 import "./loading.css";
+import "./readability.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"),
